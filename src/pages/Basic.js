e658b2d0cf4daacadd27bbd3019866fda6d42fc5
kimzerovirus <yg9699k@naver.com>
2022-04-03 23:16:32 +0900

    ammojs 적용

--- src/pages/Basic.js
+++ src/pages/Basic.js
@@ -1,7 +1,7 @@
 import * as THREE from 'three';
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls';
 
-import * as AMMO from 'Ammo.js';
+import * as AMMO from 'ammo.js';
 
 class Basic {
 	constructor(target) {
@@ -41,8 +41,9 @@ class Basic {
 				solver,
 				collisionConfiguration,
 			);
-			physicsWorld.setGravity(new Ammo.btVector3(0, -9.807, 0)); // x,y,z y축에 중력의 가속도 값인 -9.807
+			physicsWorld.setGravity(new Ammo.btVector3(0, -9.807, 0));
 
+			this.Ammo = Ammo;
 			this._physicsWorld = physicsWorld;
 			this._setupModel();
 		});
@@ -63,101 +64,116 @@ class Basic {
 		table.position.set(position.x, position.y, position.z);
 		table.scale.set(scale.x, scale.y, scale.z);
 
-		// 그림자를 받아 표현하기
 		table.receiveShadow = true;
 		this._scene.add(table);
-	}
 
-	_createDomino() {
-		// 도미노의 나선형 경로 포인트
-		const controlPoints = [
-			[-10, 0, -10],
-			[10, 0, -10],
-			[10, 0, 10],
-			[-10, 0, 10],
-			[-10, 0, -8],
-			[8, 0, -8],
-			[8, 0, 8],
-			[-8, 0, 8],
-			[-8, 0, -6],
-			[6, 0, -6],
-			[6, 0, 6],
-			[-6, 0, 6],
-			[-6, 0, -4],
-			[4, 0, -4],
-			[4, 0, 4],
-			[-4, 0, 4],
-			[-4, 0, -2],
-			[2, 0, -2],
-			[2, 0, 2],
-			[-2, 0, 2],
-			[-2, 0, 0],
-			[0, 0, 0],
-		];
-
-		const p0 = new THREE.Vector3();
-		const p1 = new THREE.Vector3();
-		const curve = new THREE.CatmullRomCurve3(
-			controlPoints
-				.map((p, ndx) => {
-					if (ndx === controlPoints.length - 1) return p0.set(...p);
-					p0.set(...p);
-					p1.set(...controlPoints[(ndx + 1) % controlPoints.length]);
-					return [
-						new THREE.Vector3().copy(p0),
-						new THREE.Vector3().lerpVectors(p0, p1, 0.3),
-						new THREE.Vector3().lerpVectors(p0, p1, 0.7),
-					];
-				})
-				.flat(),
-			false,
+		const transform = new this.Ammo.btTransform();
+		const quaternion = { x: 0, y: 0, z: 0, w: 1 };
+		transform.setIdentity();
+		transform.setOrigin(
+			new this.Ammo.btVector3(position.x, position.y, position.z),
+		);
+		transform.setRotation(
+			new this.Ammo.btQuaternion(
+				quaternion.x,
+				quaternion.y,
+				quaternion.z,
+				quaternion.w,
+			),
+		);
+		const motionState = new this.Ammo.btDefaultMotionState(transform);
+		const colShape = new this.Ammo.btBoxShape(
+			new this.Ammo.btVector3(scale.x * 0.5, scale.y * 0.5, scale.z * 0.5),
 		);
 
-		const points = curve.getPoints(1000);
-		const geometry = new THREE.BufferGeometry().setFromPoints(points);
-		const material = new THREE.LineBasicMaterial({ color: 0xffff00 });
-		const curveObject = new THREE.Line(geometry, material);
+		const mass = 0;
+		colShape.calculateLocalInertia(mass);
 
-		this._scene.add(curveObject);
+		const rbInfo = new this.Ammo.btRigidBodyConstructionInfo(
+			mass,
+			motionState,
+			colShape,
+		);
 
-		// 도미노 한개의 크기
-		const scale = { x: 0.75, y: 1, z: 0.1 };
+		const body = new this.Ammo.btRigidBody(rbInfo);
+		this._physicsWorld.addRigidBody(body);
+	}
 
-		const dominoGeometry = new THREE.BoxGeometry();
-		const dominoMaterial = new THREE.MeshNormalMaterial();
+	_createBox() {
+		const boxGeometry = new THREE.BoxGeometry(2, 2, 2);
+		const boxMaterial = new THREE.MeshNormalMaterial();
+		const boxMesh = new THREE.Mesh(boxGeometry, boxMaterial);
+		boxMesh.position.y = 10;
+		this._scene.add(boxMesh);
 
-		const step = 0.0001;
-		let length = 0.0;
-		for (let t = 0; t < 1.0; t += step) {
-			// 커브 상의 위치는 getPoint메서드를 통해 얻어오며,
-			// 인자는 0 ~ 1의 값을 가진다
-			// 0은 커브의 시작점
-			// 0.5는 커브의 중간
-			// 1은 커브의 마지막점
-			const pt1 = curve.getPoint(t); // 커브의 t에 대한 위치
-			const pt2 = curve.getPoint(t + step); // pt1 다음 위치
+		const transform = new this.Ammo.btTransform();
+		const quaternion = { x: 0, y: 0, z: 0, w: 1 };
 
-			length += pt1.distanceTo(pt2); // pt1과 pt2 사이의 누적거리
+		transform.setIdentity();
+		transform.setOrigin(new this.Ammo.btVector3(0, 10, 0));
+		transform.setRotation(
+			new this.Ammo.btQuaternion(
+				quaternion.x,
+				quaternion.y,
+				quaternion.z,
+				quaternion.w,
+			),
+		);
 
-			// 누적거리가 0.4 이상일 때 장면에 추가한다.
-			if (length > 0.4) {
-				const domino = new THREE.Mesh(dominoGeometry, dominoMaterial);
-				domino.position.copy(pt1);
-				domino.scale.set(scale.x, scale.y, scale.z);
-				domino.lookAt(pt2);
+		const motionState = new this.Ammo.btDefaultMotionState(transform);
+		const colShape = new this.Ammo.btBoxShape(new this.Ammo.btVector3(2, 2, 2));
 
-				domino.castShadow = true;
-				domino.receiveShadow = true;
-				this._scene.add(domino);
+		const mass = 5;
 
-				length = 0.0;
-			}
+		colShape.calculateLocalInertia(mass);
+
+		const rbInfo = new this.Ammo.btRigidBodyConstructionInfo(
+			mass,
+			motionState,
+			colShape,
+		);
+
+		const body = new this.Ammo.btRigidBody(rbInfo);
+		this._physicsWorld.addRigidBody(body);
+
+		boxMesh.physicsBody = body;
+	}
+
+	update() {
+		const deltaTime = new THREE.Clock().getDelta();
+
+		if (this._physicsWorld) {
+			this._physicsWorld.stepSimulation(deltaTime);
+
+			this._scene.traverse(obj3d => {
+				if (obj3d instanceof THREE.Mesh) {
+					const objThree = obj3d;
+					const objAmmo = objThree.physicsBody;
+					if (objAmmo) {
+						const motionState = objAmmo.getMotionState();
+						if (motionState) {
+							let tmpTrans = this._tmpTrans;
+							if (tmpTrans === undefined) {
+								tmpTrans = new this.Ammo.btTransform();
+								this._tmpTrans = tmpTrans;
+							}
+							motionState.getWorldTransform(tmpTrans);
+
+							const pos = tmpTrans.getOrigin();
+							const quat = tmpTrans.getRotation();
+
+							objThree.position.set(pos.x(), pos.y(), pos.z());
+							objThree.quaternion.set(quat.x(), quat.y(), quat.z(), quat.w());
+						}
+					}
+				}
+			});
 		}
 	}
 
 	_setupModel() {
 		this._createTable();
-		this._createDomino();
+		this._createBox();
 	}
 
 	_setupCamera() {
@@ -195,6 +211,7 @@ class Basic {
 
 	render() {
 		this._renderer.render(this._scene, this._camera);
+		this.update();
 
 		requestAnimationFrame(this.render.bind(this));
 	}
